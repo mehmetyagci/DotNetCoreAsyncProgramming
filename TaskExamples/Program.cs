63e8eaaeaeb20b6b31062886d2d3d38b637fc0bf
Mehmet Yağcı <mehmetyagci53@gmail.com>
2022-09-05 19:38:43 +0300

    Section 2:Task 6.Task Method:ContinueWith

--- TaskExamples/Program.cs
+++ TaskExamples/Program.cs
@@ -17,12 +17,25 @@ namespace TaskExamples
 
             // await myTask;
 
-            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
-            Console.WriteLine("arada yapılacak işler");
-            var data = await myTask;
-            Console.WriteLine("data uzunluk:" + data.Length);
+            //var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+            //Console.WriteLine("arada yapılacak işler");
+            //var data = await myTask;
+            //Console.WriteLine("data uzunluk:" + data.Length);
+
+            Console.WriteLine("Hello World!");
+
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com")
+                .ContinueWith(calis);
+
+            Console.WriteLine("Arada yapılacak işler");
+
+            await myTask;
         }
 
-
+        private static void calis(Task<string> data)
+        {
+            Console.WriteLine("data uzunluk:" + data.Result.Length);
+            /// 100 satırlık kod burada
+        }
     } // end of class
 } // end of namespace
