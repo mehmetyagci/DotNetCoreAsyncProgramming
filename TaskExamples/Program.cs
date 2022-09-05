0842318d42976e202b562590a26cf1538bcbfec9
Mehmet Yağcı <mehmetyagci53@gmail.com>
2022-09-05 19:34:34 +0300

    Add project files.

---
+++ TaskExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TaskExamples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            // var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith
+            //  ((data) =>
+            //{
+            //    Console.WriteLine("data uzunluk:" + data.Result.Length);
+            //});
+
+            // Console.WriteLine("arada yapılacak işler");
+
+            // await myTask;
+
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+            Console.WriteLine("arada yapılacak işler");
+            var data = await myTask;
+            Console.WriteLine("data uzunluk:" + data.Length);
+        }
+
+
+    } // end of class
+} // end of namespace
